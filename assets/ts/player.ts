f68a8d76cdaf81a8cf378a3ea72072db25a0306d
Ryan Young <ryan@youngryan.com>
2021-07-18 19:22:08 -0700

    feat: add JS inline sound player

---
+++ assets/ts/player.ts
@@ -0,0 +1,26 @@
+class SoundPlayer {
+    audio: HTMLAudioElement;
+
+    constructor() {
+        this.audio = new Audio();
+    }
+
+    play(url: string) {
+        this.audio.src = url;
+        this.audio.load();
+        this.audio.play();
+    }
+}
+
+function init() {
+    const player = new SoundPlayer();
+    const links = document.getElementsByClassName("char-line-play");
+    const handler = function(this: HTMLAnchorElement, event: Event) {
+        player.play(this.href);
+        event.preventDefault();
+    }
+    for (var i = 0; i < links.length; i++)
+        links[i].addEventListener("click", handler);
+}
+
+init();
